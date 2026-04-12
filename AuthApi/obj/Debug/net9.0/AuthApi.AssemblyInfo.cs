14723f00f4630714b566c6ec8a56e2866d68c3b7
LuchulumancoGDev <131538691+LuchulumancoGDev@users.noreply.github.com>
2026-04-12 11:25:13 +0200

    Add Fly.io deployment config and update .dockerignore
    Introduce fly.toml for Fly.io deployment settings, update .dockerignore to exclude .vs and fly.toml, and update assembly info files to reflect a new build/version.

--- AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd39fdb97303d1494832a32d4bc2a480711fef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0916e9691430e0ddd88a0387ac5fbee3f3e3b3b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
