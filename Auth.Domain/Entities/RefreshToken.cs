2bae5aa0e08dd5f4beb919e7409ae12ea42dd714
LuchulumancoGDev <131538691+LuchulumancoGDev@users.noreply.github.com>
2026-04-12 23:07:01 +0200

    Initail setup of teh application

---
+++ Auth.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace Auth.Domain.Entities;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; }
+    public DateTime ExpiryDate { get; set; }
+    public bool IsRevoked { get; set; }
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+}
---
+++ Auth.Domain/Entities/RolePermission.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Auth.Domain.Entities;
+
+public class RolePermission
+{
+    public Guid Id { get; set; }
+    public string RoleId { get; set; }
+    public IdentityRole Role { get; set; }
+    public Guid PermissionId { get; set; }
+    public Permission Permission { get; set; }
+}
