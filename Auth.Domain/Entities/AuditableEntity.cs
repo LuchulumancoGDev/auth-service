1f9c4e4c477dd1d54ffe4211bacf6f6b399d2d3e
LuchulumancoGDev <131538691+LuchulumancoGDev@users.noreply.github.com>
2026-06-20 22:56:57 +0200

    updates

--- Auth.Domain/Entities/AuditableEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace Auth.Domain.Entities;
-
-public abstract class AuditableEntity : Entity
-{
-    public new DateTime? CreatedAt { get; set; }
-    public string? CreatedBy { get; set; }
-    public new DateTime? UpdatedAt { get; set; }
-    public string? UpdatedBy { get; set; }
-}
--- Auth.Infrastructure/MultiTenancy/ITenantProvider.cs
+++
@@ -1,6 +0,0 @@
-namespace Auth.Infrastructure.MultiTenancy;
-
-public interface ITenantProvider
-{
-    Guid? TenantId { get; set; }
-}
